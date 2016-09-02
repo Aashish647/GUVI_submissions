8c1842ab4d62d76407f9957e2e8e211a215612f7
aashish647 <aashish647@gmail.com>
2016-09-02 18:34:18 +0530

    2nd

--- repeated_nums.py
+++ repeated_nums.py
@@ -1,10 +1,26 @@
+def counter(num):
+    d = {}
+    for i in num:
+        if num.count(i)>1:
+                d[i] = num.count(i)
+
+    res=""
+    for i in d:
+        if d[i]>1:
+            res.append(i)
+    print int(res)
+
+
+
 num = (raw_input().strip())
 res=""
+try:
+    num = int(num)
+except:
+    print "input should be a string"
+    exit(0)
+
 if num<0:
     print 0
 else:
-    for i in num:
-        if num.count(i)>1:
-                res.append(i)
-
-                print int(res)
+    counter(num)
