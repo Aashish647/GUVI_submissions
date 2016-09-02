b25c1939e9bb364eb65a1f6e6a5bfb54aec9a2ed
aashish647 <aashish647@gmail.com>
2016-09-02 19:08:21 +0530

    3rd

---
+++ number_eqals_index.py
@@ -0,0 +1,13 @@
+try:
+    input = map(int,raw_input().strip())
+    def finder(input)
+except:
+    print "Please enter only integers"
+
+def finder(ip):
+    for i in ip:
+        if i== ip.index(i):
+            print i
+            exit(0)
+    print "No integer found"
+    exit(0)
